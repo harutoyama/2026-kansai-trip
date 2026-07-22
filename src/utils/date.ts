648b7761a695b710e942641fc73db26d456d4a91
Haru Tohyama <135804174+harutoyama@users.noreply.github.com>
2026-07-22 15:37:31 +0900

    fix: use compact Japanese weekday labels

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,6 @@
 export function formatJapaneseWeekday(date: string) {
-  const weekday = new Intl.DateTimeFormat('ja-JP', {
+  return new Intl.DateTimeFormat('ja-JP', {
     weekday: 'short',
     timeZone: 'Asia/Tokyo'
   }).format(new Date(`${date}T12:00:00+09:00`))
-  return `${weekday}曜日`
 }
